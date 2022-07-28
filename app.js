f8706d2cc9eb574db609f2acf862b195cb597c06
antare74 <mc.revs404@gmail.com>
2022-07-28 15:45:42 +0700

    update TCP listener and package.json scripts

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
 require('./log');
-const http = require('http');
-const url = require('url');
-const hostname = process.env.HOSTNAME || '0.0.0.0';
-const port = process.env.PORT || 3000;
-// const WebSocket = require('ws')
-// const ws = new WebSocket('ws://0.0.0.0:9898')
+const host = process.env.HOSTNAME || '0.0.0.0';
+const port = process.env.APP_PORT || 6000;
+const net = require('net');
 
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('WS TESTING\n'); 
-    req.url = url.parse(req.url, true);
-    var query = JSON.stringify(req.url.query);
-    console.log("message: " + query);
-    // if (query !== JSON.stringify({})) {
-    //     ws.send(query);
-    // }
+const server = net.createServer();
+server.listen(port, host, () => {
+    console.log('TCP Server is running on port ' + port +'.');
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+let sockets = [];
+let dateObj = new Date();
+let second = dateObj.getSeconds();
+let minute = dateObj.getMinutes();
+let hour = dateObj.getHours();
+let month = dateObj.getUTCMonth();
+let day = dateObj.getUTCDate();
+let year = dateObj.getUTCFullYear();
+let stdDate = '[' + year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second + ']';
+
+server.on('connection', function(sock) {
+    let ipAddress = sock.remoteAddress;
+    console.log(stdDate + ' ' + ipAddress + ' Connected');
+
+    sockets.push(sock);
+
+    sock.on('data', function(data) {
+        console.log(stdDate + ' ' + ipAddress + ' Data: ' + data);
+        // Write the data back to all the connected, the client will receive it as data from the server
+        sockets.forEach(function(sock, index, array) {
+            sock.write(sock.remoteAddress + ':' + sock.remotePort + " said " + data + '\n');
+        });
+    });
+
+    // Add a 'close' event handler to this instance of socket
+    sock.on('close', function(data) {
+        let index = sockets.findIndex(function(o) {
+            return o.remoteAddress === sock.remoteAddress && o.remotePort === sock.remotePort;
+        })
+        if (index !== -1) sockets.splice(index, 1);
+        console.log(stdDate + ' ' + ipAddress + ' Closed');
+    });
+
 });
--- log.js
+++ log.js
@@ -11,37 +11,13 @@ let month = dateObj.getUTCMonth();
 let day = dateObj.getUTCDate();
 let year = dateObj.getUTCFullYear();
 
-// let logFile = '';
-
-dirPath = '/logs/' + year + "-" + month;
+dirPath = '/logs/';
 fileName = '/' + year + "-" + month + "-" + day + ".log";
 logTime = hour + ":" + minute + ":" + second;
-
 const isLogExist = !fs.existsSync(path.join(__dirname, '/logs/main.log'))
 logFile = fs.createWriteStream(__dirname +  dirPath + fileName, {flags : isLogExist ?? true ? 'a' : 'w'});
-// fs.stat(path.join(__dirname, dirPath), (err, stats) => {
-//     if (err) {
-//         fs.mkdir(path.join(__dirname, dirPath), (err) => {
-//             console.error(err)
-//         });
-//         logFile = fs.createWriteStream(__dirname +  dirPath + fileName, {flags : isLogExist ?? true ? 'a' : 'w'});
-        
-//         // if (!fs.existsSync(path.join(__dirname, dirPath + fileName))) {
-//         //     logFile = fs.createWriteStream(__dirname +  dirPath + fileName, {flags : 'w'});
-//         // }else{
-//         //     logFile = fs.createWriteStream(__dirname +  dirPath + fileName, {flags : 'a'});
-//         // }
-//     }
-//     console.log = function(d) {
-//         // logFile.write(util.format(logTime) + '\n');
-//         logFile.write(util.format(d) + '\n');
-//         logStdout.write(util.format(d) + '\n');
-//     };
-// })
-
 
 console.log = function(d) {
-    // logFile.write(util.format(logTime) + '\n');
     logFile.write(util.format(d) + '\n');
     logStdout.write(util.format(d) + '\n');
 };
